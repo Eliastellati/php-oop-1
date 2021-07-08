1527cd2a28c0ea59acdf053ad1178d156770fd5e
eliastellati <eliastellatibvb@gmail.com>
2021-07-08 16:53:06 +0200

    layout

--- index.php
+++ index.php
@@ -6,7 +6,8 @@ include __DIR__ . "/partials/text.php";
 
 $movies = [
     $movie1= new Movie ("I sogni segreti di Walter Mitty", 2013, $text1, "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTiYuhoJh_waGNmobvS25k7c0Q_7W9w5icEHpaipyTK3TyepRDC"),
-    $movie2= new Movie ("Il buon, il brutto, il cattivo", 1966, $text2, "https://s3-eu-west-1.amazonaws.com/static.screenweek.it/movie/11145/poster/ita/share.jpg?1470940568")
+    $movie2= new Movie ("Il buon, il brutto, il cattivo", 1966, $text2, "https://s3-eu-west-1.amazonaws.com/static.screenweek.it/movie/11145/poster/ita/share.jpg?1470940568"),
+    $movie3= new Movie ("Call me by your name", 2019, $text3, "https://images-na.ssl-images-amazon.com/images/I/812SKExc1tL.jpg")
 ]
 
 ?>
@@ -17,6 +18,7 @@ $movies = [
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>My Movies</title>
 </head>
 <body>
---
+++ partials/text.php
@@ -0,0 +1,6 @@
+<?php
+   $text1= "Un uomo dalla vita ordinaria lavora per la rivista Life ed è segretamente innamorato della collega Cheryl. Fugge dalla propria esistenza mediocre sognando grandi imprese ad occhi aperti, ma l'improvvisa sparizione di una fotografia per l'ultima copertina del rotocalco lo spinge ad affrontare la più grande avventura della sua storia: andare alla ricerca del negativo, ovunque esso sia. Nell'impresa, viene accompagnato da un improbabile impiegato di un servizio di appuntamenti online.";
+
+   $text2 = "Mentre infuria la Guerra di Secessione, tre uomini dall'oscuro passato si battono per impossessarsi di un tesoro nascosto in un cimitero.";
+   $text3 = "L'incontro tra due ragazzi, Elio, un adolescente sensibile e istruito, e Oliver, un ventiquattrenne spontaneo, affascinante e pieno di vita, segna l'inizio di un'estate indimenticabile."
+?>
